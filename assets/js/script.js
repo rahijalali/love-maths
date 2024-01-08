d6351fd1ef1be8025befa70773af717c97666b5e
Rahi <rahi.jalali@gmail.com>
2024-01-08 10:49:51 +0000

    display the click button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,30 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("you click the answer");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        runGame(gameType);
+      }
+    });
+  }
+});
 function runGame() {}
+
 function checkAnswer() {}
+
 function calculateCorrectAnswer() {}
 
 function incrementScore() {}
 
 function incrementWrongAnswer() {}
+
 function displayAdditionQuestion() {}
 
 function displaySubtractQuestion() {}
+
 function displayMultiplyQuestion() {}
